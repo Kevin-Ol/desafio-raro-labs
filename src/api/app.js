8204d22b78677b686c6b6a9f4b1c6fd1a50f98a4
Kevin Oliveira <kevin.zero@hotmail.com>
2022-02-05 19:10:28 -0300

    Adiciona uuid e finaliza documentacao swagger

--- src/api/app.js
+++ src/api/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerDocs = require('../swagger.json');
 const errorMiddleware = require('../middlewares/error');
 const paginationRoutes = require('../routes/pagination');
 
 const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use('/v1', paginationRoutes);
 
--- src/controllers/pagination.js
+++ src/controllers/pagination.js
@@ -1,17 +1,19 @@
 const { StatusCodes } = require('http-status-codes');
+const { v4: uuidv4 } = require('uuid');
 const validatePagination = require('../validations/pagination');
 const paginationService = require('../services/pagination');
 
 module.exports = (req, res, next) => {
   const { current, total } = req.query;
 
-  const {error, currentNumber, totalNumber } = validatePagination({ current, total });
+  const { error, currentNumber, totalNumber } = validatePagination({ current, total });
 
   if (error) {
     return next(error);
   };
 
   const pagination = paginationService(currentNumber, totalNumber);
-
-  return res.status(StatusCodes.OK).json({pagination});
+  const uuid = uuidv4();
+  
+  return res.status(StatusCodes.OK).json({ uuid, pagination });
 };
